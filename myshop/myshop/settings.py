b3b5108ccb652c48cd5c663dc62a8368221ebfa4
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-07-31 08:39:23 +0530

    changes

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n^l#e9=hk46%hbp0y+6ix7+cb!-^&7=k1oyk^z1)np=(70zme_"
 
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-key-for-dev")
 
